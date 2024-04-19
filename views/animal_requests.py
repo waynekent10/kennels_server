8885374c7a486fbcb097cbed7bd45254cf2e257d
Bruce Clark <Bruce.clark55@gmail.com>
2024-04-18 20:43:45 -0400

    delete animal

--- views/animal_requests.py
+++ views/animal_requests.py
@@ -53,3 +53,18 @@ def get_single_animal(id):
             requested_animal = animal
 
     return requested_animal
+
+def delete_animal(id):
+    # Initial -1 value for animal index, in case one isn't found
+    animal_index = -1
+
+    # Iterate the ANIMALS list, but use enumerate() so that you
+    # can access the index value of each item
+    for index, animal in enumerate(ANIMALS):
+        if animal["id"] == id:
+            # Found the animal. Store the current index.
+            animal_index = index
+
+    # If the animal was found, use pop(int) to remove it from list
+    if animal_index >= 0:
+        ANIMALS.pop(animal_index)
