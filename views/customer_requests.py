3dc1e2251f250077e2ec416ac429e30d51ea0b24
Bruce Clark <Bruce.clark55@gmail.com>
2024-04-25 19:06:51 -0400

    ch3 completed

--- views/customer_requests.py
+++ views/customer_requests.py
@@ -65,7 +65,10 @@ def get_single_customer(id):
         db_cursor.execute("""
         SELECT
             a.id,
-            a.name
+            a.name,
+            a.address,
+            a.email,
+            a.password
         FROM customer a
         WHERE a.id = ?
         """, ( id, ))
@@ -97,3 +100,32 @@ def update_customer(id, new_customer):
 
             CUSTOMERS[index] = new_customer
             break
+        
+# TODO: you will get an error about the address on customer. Look through the customer model and requests to see if you can solve the issue.
+        
+def get_customer_by_email(email):
+
+    with sqlite3.connect("./kennel.sqlite3") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        # Write the SQL query to get the information you want
+        db_cursor.execute("""
+        select
+            a.id,
+            a.name,
+            a.address,
+            a.email,
+            a.password
+        from Customer a
+        WHERE a.email = ?
+        """, ( email, ))
+
+        customers = []
+        dataset = db_cursor.fetchall()
+
+        for row in dataset:
+            customer = Customer(row['id'], row['name'], row['address'], row['email'] , row['password'])
+            customers.append(customer.__dict__)
+
+    return customers
