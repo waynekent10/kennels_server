2839dde96c3895131ee06732175709c77ba85043
Bruce Clark <Bruce.clark55@gmail.com>
2024-04-26 21:09:30 -0400

    part 1 complete

--- views/animal_requests.py
+++ views/animal_requests.py
@@ -1,6 +1,6 @@
 import sqlite3
 import json
-from models import Animal
+from models import Animal, Location
 
 ANIMALS = []
 
@@ -20,8 +20,12 @@ def get_all_animals():
             a.breed,
             a.status,
             a.location_id,
-            a.customer_id
-        FROM animal a
+            a.customer_id,
+            l.name location_name,
+            l.address location_address
+        FROM Animal a
+        JOIN Location l
+            ON l.id = a.location_id
         """)
 
         # Initialize an empty list to hold all animal representations
@@ -31,17 +35,20 @@ def get_all_animals():
         dataset = db_cursor.fetchall()
 
         # Iterate list of data returned from database
-        for row in dataset:
+    for row in dataset:
 
-            # Create an animal instance from the current row.
-            # Note that the database fields are specified in
-            # exact order of the parameters defined in the
-            # Animal class above.
-            animal = Animal(row['id'], row['name'], row['breed'],
-                            row['status'], row['location_id'],
-                            row['customer_id'])
+        # Create an animal instance from the current row
+        animal = Animal(row['id'], row['name'], row['breed'], row['status'],
+                        row['location_id'], row['customer_id'])
 
-            animals.append(animal.__dict__) # see the notes below for an explanation on this line of code.
+        # Create a Location instance from the current row
+        location = Location(row['id'], row['location_name'], row['location_address'])
+
+        # Add the dictionary representation of the location to the animal
+        animal.location = location.__dict__
+
+        # Add the dictionary representation of the animal to the list
+        animals.append(animal.__dict__) # see the notes below for an explanation on this line of code.
 
     return animals
 
